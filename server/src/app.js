1c88d9805409afbc234453adfa6c4e284de02bc3
alomatee@yahoo.com <alomatee@yahoo.com>
2025-05-10 23:18:55 +0100

    completed the signup, login and change password endpoint

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,9 @@ const mongoose = require("mongoose");
 const cors = require("cors"); // For handling cross-origin requests
 require("dotenv").config(); // To manage environment variables
 const connectDB = require("./database/db");
+const authRoutes = require('./server/routes/auth.routes')
+const cookieParser = require('cookie-parser');
+
 
 
 const app = express();
@@ -11,13 +14,14 @@ const PORT = process.env.PORT || 6000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 // Connect to MongoDB
 connectDB()
 // Routes
 // app.use("/api/v1/results", resultRoutes);
 // app.use("/api/v1/student", studentRoutes);
-// app.use("/api/v1", authRoutes);
+app.use("/api/v1", authRoutes);
 
 
 app.listen(PORT, () => {
