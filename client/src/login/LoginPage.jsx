66ca0e77e3b2e753e20ed9a020db47de045e490b
oluprince <preciousoluboyo@gmail.com>
2025-05-12 13:36:54 +0100

    my first commit

---
+++ client/src/login/LoginPage.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './login.css';
+
+export const LoginPage = () => {
+  return (
+    <div>
+        <p>Login</p>
+
+        <div>
+            <h5>
+                Username
+            </h5>
+            <hr />
+        </div>
+        <div>
+            <h5>
+                Password
+            </h5>
+            <hr />
+        </div>
+        
+    </div>
+  )
+}
